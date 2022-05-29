e33fdd071afc169ee856096d92f35f61187ef9ba
ZhanatUturova <526628@gmail.com>
2022-05-30 04:30:15 +0600

    create form for feedback

--- feedback/views.py
+++ feedback/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from .forms import FeedbackForm
 
 # Create your views here.
 def index(request):
-    return render(request, 'feedback/feedback.html')
+    if request.method == 'POST':
+        form = FeedbackForm(request.POST)       # здесь форма заполнена данными
+        if form.is_valid():
+            print(form.cleaned_data)
+            return HttpResponseRedirect('/done')
+    form = FeedbackForm()           # здесь форма пустая
+    return render(request, 'feedback/feedback.html', context={'form': form})
+
+def done(request):
+    return render(request, 'feedback/done.html')
