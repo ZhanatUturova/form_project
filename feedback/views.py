89f78711040812e6d6ba51619f365838c33d1bd4
ZhanatUturova <526628@gmail.com>
2022-06-05 03:03:49 +0600

    change FeedBackView from FormView onto CreateView based

--- feedback/views.py
+++ feedback/views.py
@@ -6,32 +6,30 @@ from .models import Feedback
 from django.views import View
 from django.views.generic.base import TemplateView
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import FormView
+from django.views.generic.edit import CreateView
 
 
-# class FeedBackView(View):
-#     def get(self, request):
-#         form = FeedbackForm()  # здесь форма пустая
-#         return render(request, 'feedback/feedback.html', context={'form': form})
+# class FeedBackView(FormView):
+#     # в шаблоне html надо обязательно обращаться к переменной form!
+#     form_class = FeedbackForm  # оператор вызова не нужен!
+#     template_name = 'feedback/feedback.html'
+#     success_url = '/done'  # URL куда должно быть перенаправление при успешной обработке формы
 #
-#     def post(self, request):
-#         form = FeedbackForm(request.POST)  # здесь форма заполнена данными
-#         if form.is_valid():
-#             form.save()
-#             return HttpResponseRedirect('/done')
-#         return render(request, 'feedback/feedback.html', context={'form': form})
+#     # описываем тут, что нужно делать с данными, которые мы получили в форме
+#     def form_valid(self, form):
+#         form.save()  # проверка на валидность не нужна. Тут форма уже точно валидна
+#         return super(FeedBackView, self).form_valid(form)
 
 
-class FeedBackView(FormView):
+class FeedBackView(CreateView):
     # в шаблоне html надо обязательно обращаться к переменной form!
+    model = Feedback
+    # fields = ['name', 'surname', 'feedback']    # список полей, которые надо отобразить в форме (может вызвать ошибку, если не написать обязательное поле по модели
+    # fields = '__all__'          # чтобы отображались все поля
+    # fields надо прописывать если не использовать form_class!
     form_class = FeedbackForm  # оператор вызова не нужен!
     template_name = 'feedback/feedback.html'
     success_url = '/done'  # URL куда должно быть перенаправление при успешной обработке формы
-
-    # описываем тут, что нужно делать с данными, которые мы получили в форме
-    def form_valid(self, form):
-        form.save()  # проверка на валидность не нужна. Тут форма уже точно валидна
-        return super(FeedBackView, self).form_valid(form)
 
 
 class DoneView(TemplateView):
