7791a38179385a7470dc945a129f9742fea64afb
ZhanatUturova <526628@gmail.com>
2022-06-06 03:48:08 +0600

    change GalleryView onto CreateGalleryView

--- gallery/views.py
+++ gallery/views.py
@@ -1,27 +1,26 @@
 from django.shortcuts import render
 from django.views import View
+from django.views.generic.edit import CreateView
 from .forms import GalleryUploadForm
 from django.http import HttpResponseRedirect
 from .models import Gallery
 
 
-# def storage_file(file):
-#     with open(f'gallery_tmp/{file.name}', 'wb+') as new_file:
-#         for chunk in file.chunks():
-#             new_file.write(chunk)
+# class GalleryView(View):
+#     def get(self, request):
+#         form = GalleryUploadForm()
+#         return render(request, 'gallery/load_file.html', {'form': form})
+#
+#     def post(self, request):
+#         form = GalleryUploadForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             new_image = Gallery(image=form.cleaned_data['image'])
+#             new_image.save()
+#             return HttpResponseRedirect('load_image')
+#         return render(request, 'gallery/load_file.html', {'form': form})
 
-
-class GalleryView(View):
-    def get(self, request):
-        form = GalleryUploadForm()
-        return render(request, 'gallery/load_file.html', {'form': form})
-
-    def post(self, request):
-        form = GalleryUploadForm(request.POST, request.FILES)
-        if form.is_valid():
-            # storage_file(request.FILES['image'])      # можно так обращаться
-            # storage_file(form.cleaned_data['image'])    # и так тоже можно обращаться
-            new_image = Gallery(image=form.cleaned_data['image'])
-            new_image.save()
-            return HttpResponseRedirect('load_image')
-        return render(request, 'gallery/load_file.html', {'form': form})
+class CreateGalleryView(CreateView):
+    model = Gallery
+    form_class = GalleryUploadForm
+    template_name = 'gallery/load_file.html'
+    success_url = '/load_image'
