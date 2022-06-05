1d1d15f9c226e43082761ddfd0536aa16050335f
ZhanatUturova <526628@gmail.com>
2022-06-06 02:25:03 +0600

    add post func for GalleryView

--- gallery/views.py
+++ gallery/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from django.views import View
 
-# Create your views here.
+
+def storage_file(file):
+    with open(f'gallery_tmp/{file.name}', 'wb+') as new_file:
+        for chunk in file.chunks():
+            new_file.write(chunk)
+
+
 class GalleryView(View):
     def get(self, request):
         return render(request, 'gallery/load_file.html')
 
     def post(self, request):
-        pass
+        storage_file(request.FILES['image'])
+        return render(request, 'gallery/load_file.html')
