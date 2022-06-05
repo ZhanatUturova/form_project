cc369bc8fb0ea0cb5d3683b165f3e45beefd34df
ZhanatUturova <526628@gmail.com>
2022-06-06 02:55:18 +0600

    create Gallery model and change GalleryView

--- gallery/views.py
+++ gallery/views.py
@@ -2,12 +2,13 @@ from django.shortcuts import render
 from django.views import View
 from .forms import GalleryUploadForm
 from django.http import HttpResponseRedirect
+from .models import Gallery
 
 
-def storage_file(file):
-    with open(f'gallery_tmp/{file.name}', 'wb+') as new_file:
-        for chunk in file.chunks():
-            new_file.write(chunk)
+# def storage_file(file):
+#     with open(f'gallery_tmp/{file.name}', 'wb+') as new_file:
+#         for chunk in file.chunks():
+#             new_file.write(chunk)
 
 
 class GalleryView(View):
@@ -19,6 +20,8 @@ class GalleryView(View):
         form = GalleryUploadForm(request.POST, request.FILES)
         if form.is_valid():
             # storage_file(request.FILES['image'])      # можно так обращаться
-            storage_file(form.cleaned_data['image'])    # и так тоже можно обращаться
+            # storage_file(form.cleaned_data['image'])    # и так тоже можно обращаться
+            new_image = Gallery(image=form.cleaned_data['image'])
+            new_image.save()
             return HttpResponseRedirect('load_image')
         return render(request, 'gallery/load_file.html', {'form': form})
